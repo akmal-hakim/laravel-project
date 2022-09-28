496eeeae44bf20a78043b4160fad78f1446fa560
Muhd-Firdaus <muhammadfirdaus2730@gmail.com>
2022-09-28 22:21:23 +0800

    task1 index homepage

--- app/Http/Controllers/HomePageController.php
+++ app/Http/Controllers/HomePageController.php
@@ -9,7 +9,7 @@ class HomePageController extends Controller
     public function index()
     {
         $data['title'] = 'hi';
-        $view_name = getTheme() . '::homepage.home';
+        $view_name = getTheme() . '::homepage.index';
         return view($view_name, $data);
     }
 }
